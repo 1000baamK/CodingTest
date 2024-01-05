c9a7a1a0af7f0e074ad2ccfa69008652e06b928d
천병국 <1000baam@gmail.com>
2024-01-05 17:38:10 +0900

    [level unrated] Title: 더 크게 합치기, Time: 10.69 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String str1 = String.valueOf(a)+b;
+        String str2 = String.valueOf(b)+a;
+        
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+        
+        if(num1 >= num2){
+            answer = num1;
+        }else{
+            answer = num2;
+        }
+        
+        return answer;
+    }
+}
