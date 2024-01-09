cbfe5f62b648e3a634f8824225485a8862142ca6
천병국 <1000baam@gmail.com>
2024-01-09 11:09:48 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length+1];
+        
+        int a = num_list[num_list.length-1];
+        int b = num_list[num_list.length-2];
+        
+        for(int i=0; i<num_list.length; i++){
+            answer[i] = num_list[i];
+        }
+        
+        answer[num_list.length] = (a > b) ? a-b : a*2;
+        
+        return answer;
+    }
+}
