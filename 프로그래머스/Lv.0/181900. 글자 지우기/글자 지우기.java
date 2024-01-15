d7d2e4e09a438fb471e80ca404a33f6b8bee56d2
천병국 <1000baam@gmail.com>
2024-01-15 14:53:55 +0900

    [level Lv.0] Title: 글자 지우기, Time: 0.78 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String answer = "";
+        
+        Arrays.sort(indices);
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append(my_string);
+        
+		for(int i=indices.length-1; i>=0; i--) {
+			sb.deleteCharAt(indices[i]);
+		}
+        
+        answer = sb.toString();
+        return answer;
+    }
+}
